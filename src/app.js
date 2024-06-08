9084176e6df685f9182955d3b1749c4cdf00b33f
DevEduardoSouza <eduardosouza.din2@gmail.com>
2024-06-08 08:55:38 -0300

    feat: add link do site

--- src/app.js
+++ src/app.js
@@ -15,7 +15,19 @@ async function init(chatId) {
     const messageToSend = formattedMessages.join("\n");
 
     if (chatId) {
-      bot.sendMessage(chatId, messageToSend, { parse_mode: "Markdown" });
+      bot.sendMessage(chatId, messageToSend, {
+        parse_mode: "Markdown",
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "Clique aqui para mais informações",
+                url: "https://br.1x001.com/br/live/esports",
+              },
+            ],
+          ],
+        },
+      });
     } else {
       console.error("ChatId não definido.");
     }
@@ -24,7 +36,7 @@ async function init(chatId) {
   }
 }
 
-bot.onText(/\/startcs2/, (msg) => {
+bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, "O bot está ativo e monitorando jogos de cs2!");
   setInterval(() => init(chatId), 60000);
